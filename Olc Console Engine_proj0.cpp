9dd00f75d11e9d28672525f04462872dee5f4ce9
Blake Ba <blake.elink@gmail.com>
2020-04-22 12:21:07 -0400

    rule 30 is working

--- Olc Console Engine_proj0.cpp
+++ Olc Console Engine_proj0.cpp
@@ -4,10 +4,12 @@
 
 #include "olcConsoleGameEngine.h"
 #include <iostream>
+#include <cassert>
+
 using namespace std;
 
-const int WIDTH = 120;
-const int HEIGHT = 90;
+const int WIDTH = 140;
+const int HEIGHT = 120;
 // first 3 bools are the values of the 3 blocks in the prev generation
 // fourth bool is the value of the middle block in next generation
 
@@ -15,11 +17,12 @@ const int HEIGHT = 90;
 bool rule1[3]{true, true, true};
 bool rule2[3]{true, true, false};
 bool rule3[3]{true, false, true};
-bool rule4 [3] { false, false, false };
+bool rule4[3] { false, false, false };
 // result is 1 in the middle block below
-bool rule5[3]{false, true, true};
-bool rule6[3]{false, true, false};
-bool rule7[3]{false, false, true};
+bool rule5[3]{true, false, false};
+bool rule6[3]{false, true, true};
+bool rule7[3]{false, true, false};
+bool rule8[3]{false, false, true};
 
 struct Line {
   bool cell[WIDTH]{};
@@ -45,7 +48,7 @@ class Example : public olcConsoleGameEngine {
 
     // create next generations
     for (int i = 1; i < HEIGHT; i++) {
-      lines[i] = create_new_line(lines[i - 1]);
+      lines[i] = create_new_from_prev(lines[i - 1]);
     }
   };
 
@@ -53,8 +56,7 @@ class Example : public olcConsoleGameEngine {
     // draw all the lines
     for (int yi = 0; yi < HEIGHT; yi++)
       draw_line(lines[yi], yi) ;
-
-    return true;
+      return true;
   }
 
   void draw_line(Line& l, int ycoord) {
@@ -63,10 +65,32 @@ class Example : public olcConsoleGameEngine {
         Draw(i, ycoord);
   }
 
-  Line create_new_line(const Line& prev) { 
-    Line nextLine = Line(); 
-    nextLine = prev;
-    return nextLine;
+  Line create_new_from_prev(Line& prev) { 
+    // create and initialize a new line object
+    Line next = Line(); 
+
+    // loop through in chunks of 3
+    for (int i = 1; i < WIDTH - 2; i = i + 1) {
+      // apply first 4 rules
+      int rules_that_matched = 0;
+      for (auto& rule : {rule1, rule2, rule3, rule4}) {
+        if (prev.cell[i - 1] == rule[0] && prev.cell[i] == rule[1] &&
+            prev.cell[i + 1] == rule[2]) {
+          next.cell[i] = false;
+          rules_that_matched++;
+        }
+      }
+      // apply last 3 rules
+      for (auto& rule : {rule5, rule6, rule7, rule8}) {
+        if (prev.cell[i - 1] == rule[0] && prev.cell[i] == rule[1] &&
+            prev.cell[i + 1] == rule[2]) {
+          next.cell[i] = true;
+          rules_that_matched++;
+        }
+      }
+      assert(rules_that_matched == 1);
+    }
+    return next;
   }
 
   bool OnUserUpdate(float fElapsedTime) { return true; }
